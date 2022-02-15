4daaacebd78e22e21866ff5ac022b0b77417dd22
TeoVH <villadamateo64@gmail.com>
2022-02-14 20:05:20 -0800

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x00-hello_world/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	int unidades;
+	int decimales;
+
+	for (decimales = '0'; decimales <= '9'; decimales++)
+	{
+		for (unidades = (decimales + 1); unidades <= '9'; unidades++)
+		{
+			putchar(decimales);
+			putchar(unidades);
+
+			if (decimales != '8' || unidades != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
