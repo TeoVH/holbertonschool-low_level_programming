efa7f53eb4b07053009f8a3b386ef89efeecfa3d
TeoVH <villadamateo64@gmail.com>
2022-02-16 10:04:42 -0800

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *int _islower(int c)
+ *
+ *c is a parameter
+ *
+ * return none
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
