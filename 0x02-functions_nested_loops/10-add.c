93d99e3dcafd0286ee8fcbe2e036392da229e1c7
TeoVH <villadamateo64@gmail.com>
2022-02-17 12:01:01 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: Is a parameter
+ * @b: Is a parameter
+ * Return: Always c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
