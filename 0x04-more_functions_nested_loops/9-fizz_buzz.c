2b2231ffe99f0afaf9347e0169da251e7f71f191
TeoVH <villadamateo64@gmail.com>
2022-02-18 13:01:10 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; ++i)
 	{
-		if(i % 3 == 0)
+		if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		if(i % 5 == 0)
+		if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		if((i % 3 !=0) && (i % 5 != 0))
+		if ((i % 3 != 0) && (i % 5 != 0))
 		{
-			printf("%d",i);
+			printf("%d", i);
 		}
 		printf(" ");
 	}
